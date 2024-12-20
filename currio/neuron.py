345dff5621355d458585008d77a2a7d4dc22b061
Michael Flaks <misha.flaks@gmail.com>
2024-12-20 03:21:53 +0100

    Add .calculate_currents method to update record dictionary with currents

--- currio/neuron.py
+++ currio/neuron.py
@@ -3,16 +3,16 @@ import pyvista as pv
 
 import os
 
+from scipy.interpolate import interp1d
+
 from currio import __modelpath__
+from currio.utils import interp_along_axis
 
 
 class Neuron(object):
     def __init__(self, model_name=None, hocfile='mosinit.hoc'):
         from neuron import h, nrn
-        from neuron import h
-        from neuron import h, nrn
         self.h = h
-        self.nrn = nrn
         self.nrn = nrn
         self.hocfile = hocfile
         
@@ -35,12 +35,6 @@ class Neuron(object):
         A dictionary with keys being names of sections. Values are dictionaries
         each containing the 3D coordinates of the neuron model per section. The
         keys of the dictionary are:
-        A list of dictionaries, each containing the 3D coordinates of the neuron model per section.
-        The keys of the dictionary are:
-            - `id`: the name of the section
-        A dictionary with keys being names of sections. Values are dictionaries
-        each containing the 3D coordinates of the neuron model per section. The
-        keys of the dictionary are:
             - `x`: the x-coordinates of segments
             - `y`: the y-coordinates of segments
             - `z`: the z-coordinates of segments
@@ -57,14 +51,10 @@ class Neuron(object):
             - `proc_name`: the name of the procedure used to run the simulation.
             - `t`: the time vector 
             
-            For each section in h.allsec():
+            A key `sec_name` for each section in h.allsec():
             - `sec_name`: a dictionary with keys:
                 - `v`: a list of voltage traces `_ref_v` for each segment in the section, i.e.
                     `v[i]` is the voltage trace for the `i`-th segment in the section of length `len(t)`.
-                - `var_i`: a list of tracked variable passed to the `simulate` function as an argument
-                    `record` as `.simulate(record=['var_1', 'var_2', ...])` with the same structure as `v`.
-                - `var_i`: a list of tracked variable passed to the `record` function as `.record(track=['var_1', 'var_2', ...])`
-                    with the same structure as `v`.
                 - `var_i`: a list of tracked variable passed to the `simulate` function as an argument
                     `record` as `.simulate(record=['var_1', 'var_2', ...])` with the same structure as `v`.
                 - `sec`: a reference to the section object in NEURON.
@@ -75,19 +65,11 @@ class Neuron(object):
         Used to plot the 3D model of the neuron, to compute arc lengths along sections and 
         to store values for visualization."""
         
-        self.mesh: pv.MultiBlock = None
-        """PyVista mesh of the neuron model. Updated by `.create_3d_mesh()` method. 
-        Used to plot the 3D model of the neuron, to compute arc lengths along sections and 
-        to store values for visualization."""
-        
     def load_model(self, model_name):
         os.chdir(self.model_path)
         self.h.load_file(self.hocfile)
         return self
-        return self
         
-    def simulate(self, proc_name, force=False, record=None):
-    def simulate(self, proc_name, force=False):
     def simulate(self, proc_name, force=False, record=None):
         """Run a simulation procedure defined in the model .hoc file."""
         
@@ -100,15 +82,10 @@ class Neuron(object):
                 raise ValueError("Simulation already run. Use `force=True` to run again.")
             
         self.create_record(proc_name=proc_name, record=record)
-        self.create_record(proc_name=proc_name)
-        self.create_record(proc_name=proc_name, record=record)
         getattr(self.h, proc_name)()
         self.convert_record()
         return self
-        return self
         
-    def create_record(self, proc_name, record=None):
-    def create_record(self, proc_name):
     def create_record(self, proc_name, record=None):
         """Creates a record of the simulation. Record is a dictionary with keys 
         as section names and values as dictionaries containing the voltage traces and 
@@ -130,7 +107,6 @@ class Neuron(object):
         # Add recording to the records list
         self.records.append(record)
         return self
-        return self
         
     def convert_record(self):
         """Converts the record dictionary from HocObjects to np.arrays."""
@@ -149,13 +125,72 @@ class Neuron(object):
                 else:
                     # Each x is a HocObject of recorded values with length len(t), convert to np.array
                     sec_dict[key] = np.array([x.as_numpy() for x in sec_dict[key]])
-                    continue
-                else:
-                    # Each x is a HocObject of recorded values with length len(t), convert to np.array
-                    sec_dict[key] = np.array([x.as_numpy() for x in sec_dict[key]])
         
         self.record = record
         self.records[-1] = record
+        return self
+        
+    def calculate_currents(self):
+        """Calculate the currents in the neuron model.
+        
+        Propagates the self.record dictionary with new keys:
+            - `I`: a list of currents for each segment in the section of length `len(t)`.
+        """
+        if self.record is None:
+            raise ValueError(
+                """No simulation record found. Run a simulation first by specifying
+                    the process name in `.simulate('proc_name')`.""")
+        else:
+            record = self.record
+            
+        if self.mesh is None:
+            mesh = self.create_3d_mesh().mesh
+        else:
+            mesh = self.mesh
+        
+        if self.data_3d is None:
+            data_3d = self.load_3d_model().data_3d
+        else:
+            data_3d = self.data_3d
+        
+        for key, sec_dict in record.items():
+            if key == "t" or key == "proc_name":
+                continue
+            
+            sec = sec_dict["sec"]
+            voltages = sec_dict["v"]
+            diameters = data_3d[key]["d"]
+            
+            polydata = mesh[key]
+            
+            # `v_dists` are distances along the section where voltages are calculated,
+            # whereas `seg_points` are points from the 3D model of the neuron.
+            # They are different and voltages need to be interpolated to the 3D model points.
+            v_dists = np.array([seg.x for seg in sec.allseg()]) * sec.L
+            seg_lengths = np.array(polydata.field_data["arc_length"])
+            seg_dists = np.cumsum(seg_lengths)
+            
+            interp_v = interp_along_axis(
+                voltages, 
+                v_dists,
+                seg_dists,
+                axis=0)
+
+            axial_resistance = sec.Ra  # axial resistance in Ohm*cm
+            axial_resistance *= 1e4    # convert to Ohm*um
+            
+            # Cross-sectional area of the 3D segments
+            middle_diameters = (diameters[:-1] + diameters[1:])  / 2  # find the middle diameters
+            
+            
+            area = middle_diameters ** 2 * np.pi / 4 # cross-sectional area in um^2
+            axial_resistance_per_units_length = axial_resistance / area
+            
+            currents = np.diff(interp_v, axis=0) / (seg_lengths[1:] * axial_resistance_per_units_length)[:, None]
+            record[key]["interp_v"] = interp_v
+            record[key]["currents"] = currents
+        return self
+
 
     def load_3d_model(self):
         data = {}
--- currio/utils.py
+++ currio/utils.py
@@ -3,98 +3,25 @@ import numpy as np
 import pyvista as pv
 from scipy.interpolate import interp1d
 
-def load_model(model_name):
-    h.load_file(f"{model_name}/mosinit.hoc")
-    return h
-
-def extract_vt():
-    vt_dict = {}
-    vt_dict["t"] = h.Vector().record(h._ref_t)
-    for sec in h.allsec():
-        vt_dict[sec.name()] = {"v": []}
-        for seg in sec.allseg():
-            v = h.Vector().record(seg._ref_v)
-            vt_dict[sec.name()]["v"].append(v)
-            vt_dict[sec.name()]["sec"] = sec
-    h.superrun()
-    return vt_dict
-
-def explode_3d_vt_dict(vt_dict):
-    res_dict = {}
-    for key, sec_dict in vt_dict.items():
-        if key == "t":
-            res_dict["t"] = sec_dict.as_numpy()
-            continue
-        sec = sec_dict["sec"]
-        diameters = np.array([sec.diam3d(i) for i in range(sec.n3d())])
-        voltages = np.array([v.as_numpy() for v in sec_dict["v"]])
-        x = np.array([seg.x for seg in sec.allseg()]) * sec.L
-        arc3d = get_arc_lengths(sec, cumulative=True)
-        pts3d = np.array([[sec.x3d(i), sec.y3d(i), sec.z3d(i)] for i in range(sec.n3d())])
-        newvoltages = interp_along_axis(voltages, x, arc3d, axis=0)
-        res_dict[key] = {"v": newvoltages, "pts3d": pts3d, "diam": diameters, "arc3d": arc3d, "sec": sec}
-    return res_dict
-
-def get_arc_lengths(sec, cumulative=False):
-    lengths = np.empty((sec.n3d(), ))
-    L = sec.L
-    x, y, z = sec.x3d(0), sec.y3d(0), sec.z3d(0)
-    r = np.array([x, y, z])
-    lengths[0] = 0.
-    for i in range(sec.n3d()-1):
-        p = np.array([sec.x3d(i+1), sec.y3d(i+1), sec.z3d(i+1)])
-        l = np.linalg.norm(p - r)
-        r = p
-        if cumulative:
-            lengths[i+1] = lengths[i] + l
-        else:
-            lengths[i+1] = l
-    if cumulative and lengths[-1] - L > 1e-6:
-        lengths[-1] = L
-    return lengths
 
 def interp_along_axis(y, x, newx, axis):
+    """Interpolate along a given axis of an array. Given an array y with shape (N, M), 
+    array x with shape N and newx with shape L, this function interpolates y along the axis 0
+    and outputs a newy array with shape (L, M). The interpolation is done using a linear interpolation.
+    
+    In general, if y is of shape (N, ..., K, ...), x should be of shape (K,) and newx of shape (L,) to give
+    a newy of shape (N, ..., L, ...).
+
+    This function assumes that the x-xoordinate increases monotonically
+    """
+
     if y.shape[axis] != x.shape[0]:
         raise ValueError("y and x must have the same length along the axis to interpolate")
+    
     newy_shape = list(y.shape)
     newy_shape[axis] = len(newx)
     newy = np.empty(newy_shape)
+
     newy = np.apply_along_axis(lambda y: interp1d(x, y, kind='linear', fill_value='extrapolate')(newx), axis, y)
-    return newy
-
-def extract_neuron_3d_model():
-    data = []
-    for sec in h.allsec():
-        xs = [sec.x3d(i) for i in range(sec.n3d())]
-        ys = [sec.y3d(i) for i in range(sec.n3d())]
-        zs = [sec.z3d(i) for i in range(sec.n3d())]
-        ds = [sec.diam3d(i) for i in range(sec.n3d())]
-        data.append({
-            "id": sec.hname(),
-            "x": xs,
-            "y": ys,
-            "z": zs,
-            "d": ds
-        })
-    return data
-
-def convert_to_pyvista_mesh(neuron_data):
-    points = []
-    lines = []
-    diameters = []
-    for sec_data in neuron_data:
-        n_points = len(sec_data["x"])
-        for i in range(n_points):
-            points.append([sec_data["x"][i], sec_data["y"][i], sec_data["z"][i]])
-            diameters.append(sec_data["d"][i])
-            if i < n_points - 1:
-                lines.append([2, len(points) - 1, len(points)])
-    points = np.array(points)
-    lines = np.array(lines)
-    diameters = np.array(diameters)
     
-    polydata = pv.PolyData()
-    polydata.points = points
-    polydata.lines = lines
-    polydata["diameter"] = diameters
-    return polydata.tube(radius=0.5, scalars="diameter", capping=True, absolute=True)
+    return newy

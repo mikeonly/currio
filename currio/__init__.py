7c217463787fbe088e69593df6b354f5909ce69b
Michael Flaks <misha.flaks@gmail.com>
2025-03-05 14:34:54 +0100

    Update __init__ to expose Sensor, Neuron, IO

--- currio/__init__.py
+++ currio/__init__.py
@@ -5,3 +5,9 @@ __modelpath__ = pathlib.Path(__file__).parent.parent.resolve() / "models"
 
 __resultpath__ = pathlib.Path(__file__).parent.parent.resolve() / "results"
 """Standard path to the directory where results of a simulation are stored."""
+
+from currio.neuron import Neuron
+from currio.io import IO
+from currio.sensor import Sensor
+
+__all__ = ["Neuron", "IO", "Sensor", "utils"]
